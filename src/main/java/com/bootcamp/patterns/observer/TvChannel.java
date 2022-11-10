b114bde8be41e81f596769f204d61dc88ae15feb
definitelynotnes <nestorjohntenorio@gmail.com>
2022-11-10 13:10:54 +0800

    Added observer

---
+++ src/main/java/com/bootcamp/patterns/observer/TvChannel.java
@@ -0,0 +1,13 @@
+package com.bootcamp.patterns.observer;
+
+import com.bootcamp.patterns.observer.interfaces.Channel;
+
+public class TvChannel implements Channel{
+
+    @Override
+    public void receive(String news) {
+        System.out.println("TV Channel: " + news);
+        
+    }
+
+}
